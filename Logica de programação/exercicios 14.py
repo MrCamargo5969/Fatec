85cbc2d10a42463e54a307a4a4a083438a88d4a8
Eduardo Camargo <jesc_secret@protonmail.com>
2024-03-25 14:53:24 -0300

    .

--- Logica de programação/exercicios 14.py
+++ Logica de programação/exercicios 14.py
@@ -1,13 +1,18 @@
-a = int(input("Informe um valor -> "))
-b = int(input("Informe um valor -> "))
-c = int(input("Informe um valor -> "))
+while True:
+    print("Digite os lados do triangulo")
+    a = int(input("Informe um valor -> "))
+    b = int(input("Informe um valor -> "))
+    c = int(input("Informe um valor -> "))
 
-if a<b+c and c<b+a and b<a+c:
-    if a == b == c:
-        print("Triangulo Equilatero")
-    elif a == b or a == c or c == b:
-        print("Triangulo isóscele")
-    elif a != b and a != c and c != b:
-        print("Triangulo Escaleno")
-else:
-    print("digite os lados do triangulo")
+    if a<b+c and c<b+a and b<a+c:
+        if a == b == c:
+            print("Triangulo Equilatero")
+            break
+        elif a == b or a == c or c == b:
+            print("Triangulo isóscele")
+            break
+        elif a != b and a != c and c != b:
+            print("Triangulo Escaleno")
+            break
+    else:
+        continue

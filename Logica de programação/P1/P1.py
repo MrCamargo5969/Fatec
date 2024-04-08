3bb56ad94e71e26729a0d6e382cc4650a0b12a37
Eduardo Camargo <jesc_secret@protonmail.com>
2024-04-08 16:02:10 -0300

    go

---
+++ Logica de programação/P1/P1.py
@@ -0,0 +1,24 @@
+# Exercicio 4
+# print('Digite dois numeros inteiros:')
+# num1 = input('Primeiro numero: ')
+# num2 = input('Segundo numero: ')
+
+# if num1.isalpha() or num2.isalpha():
+#     print('Digite numeros inteiros validos')
+# else:
+#     if int(num1) % 2 == 0:
+#         num3 = int(num1) + int(num2)
+#         print(f'A soma de {num1} e {num2} é igual a {num3}')
+#     else:
+#         num3 = int(num1) - int(num2)
+#         print(f'A subtração de {num1} e {num2} é igual a {num3}')
+
+#Exercicio 5
+# nome = 'Luis'
+# idade = 10
+# valor = 40
+
+# print(nome,"tem",idade,"anos e apenas R$",valor,"no bolso")
+# print("%s tem %d anos e apenas R$ %.f no bolso" %(nome, idade, valor))
+# print("{} tem {} anos e apenas R$ {} no bolso".format(nome,idade,valor))
+

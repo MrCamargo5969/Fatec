0276f013ce56cab0ad604e1942b217ed2b65e2ca
Eduardo Camargo <jesc_secret@protonmail.com>
2024-04-22 16:20:16 -0300

    go

--- Logica_de_programacao/exercicios_35.py
+++ Logica_de_programacao/exercicios_35.py
@@ -1,8 +1,8 @@
 import random
-
 lista = []
 for i in range(20):
     num = random.randint(1, 100)
     lista.append(num)
+    print(num)
 maior = sorted(lista)
-print(maior[19])
+print(f'o maior numero é {maior[19]}')
